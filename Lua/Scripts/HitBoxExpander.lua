380bc0f6ae1e8f045de325b0236b9621f1ea2686
DraxWare <77240528+DraxWare@users.noreply.github.com>
2024-04-12 06:01:22 -0400

    Update HitBoxExpander.lua

--- Lua/Scripts/HitBoxExpander.lua
+++ Lua/Scripts/HitBoxExpander.lua
@@ -278,7 +278,7 @@ coroutine.wrap(MPIEAA_fake_script)()
 local function HGRLFB_fake_script() -- SliderMainFrame.Dragging 
 	local script = Instance.new('LocalScript', SliderMainFrame)
 
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Jonatanortiz2/Johnny-Test/master/AntiCoreGui"))();
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/DraxWare/github.io/main/Lua/Scripts/Bypass%20Anti%20Cheat.lua"))();
 	--[[
 		This Loadstring It's Going To Be Reffered To Exploit Anti Core Gui
 	]]
