ed0e6400e6c80665440a79513ddefc4236d0d6af
DraxWare <77240528+DraxWare@users.noreply.github.com>
2026-01-03 17:46:21 -0500

    Update Source.lua

--- Lua/Ui Libraries/Jons-Ui-Library/Source.lua
+++ Lua/Ui Libraries/Jons-Ui-Library/Source.lua
@@ -76,9 +76,8 @@ if identifyexecutor and type(identifyexecutor) == "function" then
     elseif Executor == 'Wave' then
         CoreGui = Services.CoreGui
     elseif Executor == 'Solara' then
-		Services=setmetatable({},{__index=function(b,c)return game:GetService(c)end})
-		CoreGui = Services.CoreGui
-		warn('Solara Detected, You\'re running with a *Fixed* version of draxware to run on solara, may be detectable! consider switching to a different executor that supports more functions!')
+		CoreGui = game.CoreGui
+		warn('Solara Detected, You\'re running with a *Fixed* version of '..HubName..' to run on solara, may be detectable! consider switching to a different executor that supports more functions!')
     elseif Executor == 'Zenith' then
 		CoreGui = gethui()
 	else
